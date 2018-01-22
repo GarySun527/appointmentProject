1cfb132763631920290b207a7a5a15b8dcb78ba1
gary <garysun527@gmail.com>
2018-01-22 15:38:52 -0500

    'init'

--- appointmentPro/settings.py
+++ appointmentPro/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'appointmentProApp,'
+    'appointmentProApp',
 ]
 
 MIDDLEWARE_CLASSES = [
